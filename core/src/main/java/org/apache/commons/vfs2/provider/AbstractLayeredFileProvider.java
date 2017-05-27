abashev/vfs-s3

Fix generating cache key for file system

commons-vfs/core/src/main/java/org/apache/commons/vfs2/provider/AbstractLayeredFileProvider.java
@@ -85,7 +85,7 @@ public abstract class AbstractLayeredFileProvider
         {
             // Create the file system
             fs = doCreateFileSystem(scheme, file, fileSystemOptions);
-            addFileSystem(rootName, fs);
+            addFileSystem(rootName, fileSystemOptions, fs);
         }
         return fs.getRoot();
     }

commons-vfs/core/src/main/java/org/apache/commons/vfs2/provider/AbstractOriginatingFileProvider.java
@@ -116,7 +116,7 @@ public abstract class AbstractOriginatingFileProvider
 
                     fs = doCreateFileSystem(rootName, fileSystemOptions);
 
-                    addFileSystem(rootName, fs);
+                    addFileSystem(rootName, fileSystemOptions, fs);
                 }
             } finally {
                 createFileSystemLock.unlock();

softace/commons-vfs

core/src/main/java/org/apache/commons/vfs2/provider/AbstractLayeredFileProvider.java
@@ -85,7 +85,7 @@ public abstract class AbstractLayeredFileProvider
         {
             // Create the file system
             fs = doCreateFileSystem(scheme, file, fileSystemOptions);
-            addFileSystem(rootName, fs);
+            addFileSystem(rootName, fs, fileSystemOptions);
         }
         return fs.getRoot();
     }

core/src/main/java/org/apache/commons/vfs2/provider/AbstractOriginatingFileProvider.java
@@ -120,7 +120,7 @@ public abstract class AbstractOriginatingFileProvider
 
                     fs = doCreateFileSystem(rootName, fileSystemOptions);
 
-                    addFileSystem(rootName, fs);
+                    addFileSystem(rootName, fs, fileSystemOptions);
                 }
             }
             finally